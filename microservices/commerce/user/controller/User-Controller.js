1f561a92bdfa64ddfb3a9f638d4c354975593e2d
biboking-373 <vinnikinyua7@gmail.com>
2026-02-28 13:21:49 +0300

    refined-the-user-service-also-added-jwtand_login

--- microservices/commerce/user/controller/User-Controller.js
+++ microservices/commerce/user/controller/User-Controller.js
@@ -1,88 +1,22 @@
-const db = require("../config/Sql-database");
+const { registerUser, loginUser } = require('../service/User-Service');
 
-exports.createUser = async (req,res) => {
-    const { name, phonenumber, age, identity } = req.body;
+const register = async (req, res) => {
+try {
+    const user = await registerUser(req.body);
+    res.status(201).json({ message: 'User registered successfully.', user });
+} catch (error) {
+    res.status(400).json({ message: error.message });
+};
+};
 
-    if (!name || !phonenumber || !age || !identity){
-        return res.status(400).json({message: "Field must be entered"});
-    }
 
-    const regexphonenumber = /^(07|01)\d{8}$/
-    if (!regexphonenumber.test(phonenumber)){
-        return res.status(400).json({message: "Phone number must be 10 digits or start with 07 or 01"})
-    }
-    
-    let formattedPhone = phonenumber;
-    formattedPhone = "+254" + phonenumber.substring(1);
+const login = async (req, res) => {
+  try {
+    const { token, user } = await loginUser(req.body);
+    res.status(200).json({ message: 'Login successful.', token, user });
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
 
-    if (age < 18 || age > 125){
-        return res.status(400).json({message: "Your either too young of age or non-existent"});
-    }
-
-    if (identity.length > 8){
-        return res.status(400).json({message: "Your guessing your ID, you naughty naughty"});
-    }
-    try {
-        const [existing] = await db.execute(
-            'SELECT * FROM user_service1 WHERE Phonenumber = ?',
-            [formattedPhone]
-        )
-
-        if (existing.length > 0){
-            return res.status(400).json({message:"User already exists"})
-        };
-
-        const [new_user] = await db.execute(
-            `INSERT INTO user_service1  (Name, Phonenumber, Age, IdentityCardnumber)
-            VALUES (?, ?, ?, ?)`,
-            [name, formattedPhone, age, identity]
-        );
-
-        res.status(201).json({
-            message: "User Created Successfully",
-            userId: new_user.insertId,
-        })
-
-    } catch(error){
-        console.error("Registration error: ", error);
-        res.status(500).json({error: "Something is wrong with the registration"});
-    }
-}
-
-exports.getUser = async(req, res) => {
-    const { phonenumber } = req.query;
-
-    if (!phonenumber) {
-        return res.status(400).json({message: "Phone-number needed"});
-    }
-
-    let formattedPhone = phonenumber;
-    if (formattedPhone.startsWith("0")){
-        formattedPhone = "+254" + formattedPhone.substring(1);
-    }
-
-    try {
-        const [rows] = await db.execute(
-            `SELECT * FROM user_service1 WHERE Phonenumber = ?`,
-            [formattedPhone]
-        );
-
-        if (rows.length === 0){
-            return res.status(400).json({message: "User not found"});
-        }
-
-        const user = rows[0];
-
-        res.json({
-            id: user.id,
-            name: user.Name,
-            phonenumber: user.Phonenumber,
-            age: user.Age,
-            IdCard: user.IdentiyCardnumber
-        });
-
-    } catch (err) {
-        console.error(" Error fetching user ", err);
-        res.status(500).json({message: " Something went wrong "});
-    }
-}
+module.exports = { register, login };
--- microservices/commerce/user/router/User-Routes.js
+++ microservices/commerce/user/router/User-Routes.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const { validateRegister, validateLogin} = require("../middleware/User-Middleware")
 const userController = require("../controller/User-Controller");
 
 
-router.post("/create-user", userController.createUser);
-router.get("/user-me", userController.getUser);
+router.post("/create-user", 
+    validateRegister,
+    userController.register,
+);
+
+router.post("/login",
+    validateLogin,
+    userController.login,
+);
+
 module.exports = router;
