b83e116fef0efe8e73f3c085295e96c735165491
Vincent Kinyua Waruiru <132344163+biboking-373@users.noreply.github.com>
2026-03-29 15:02:14 +0300

    Delete microservices/commerce/product directory

--- microservices/commerce/product/router/ProductRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router();
-
-const Productcontroller = require("../controller/ProductController");
-
-router.post("/product-mine", Productcontroller.addProduct);
-
-
-module.exports = router;
