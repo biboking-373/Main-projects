28855ee6d9407d1a2bb24eddf2a4b38f90aa2fe4
Vincent Kinyua Waruiru <132344163+biboking-373@users.noreply.github.com>
2026-03-29 15:01:43 +0300

    Delete microservices/commerce/order directory

--- microservices/commerce/order/router/order.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const Ordercontroller = require("../controller/order");
-
-router.post("/orderup", Ordercontroller.createOrder);
-
-module.exports = router;
